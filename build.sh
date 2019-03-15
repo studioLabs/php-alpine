a7dbb329e9166e62d4ee77900429559a1483d853
Steed <steed@studiolabs.io>
2019-03-15 14:59:38 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -16,8 +16,5 @@ export IMAGE_NAME=$IMAGE:$TAG && \
              docker build --no-cache --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
              --file Dockerfile \
              --build-arg VCS_REF=$COMMIT_HASH \
-             --build-arg DOCKER_REPO=$DOCKER_REPO \
-             --build-arg IMAGE_NAME=$IMAGE_NAME \
-             --build-arg IMAGE_COMMIT=$IMAGE_COMMIT \
              --squash --force-rm --compress --rm \
              -t $IMAGE_NAME .
